aba4f4aa99a917394e0362a933a970828b97e22c
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-12-01 21:11:48 +0000

    assignment done, not styled

--- my_react_app/src/TextForCards.test.js
+++ my_react_app/src/TextForCards.test.js
@@ -3,7 +3,12 @@ import TextForCards from './TextForCards';
 
 test('Text shows', () => {
     render(<TextForCards cardNum={true} />);
-    const linkElement = screen.getByText(/My Card/i);
+    const linkElement = screen.getByText(/Text from card/i);
     expect(linkElement).toBeInTheDocument();
 });
 
+test('Text shows', () => {
+    render(<TextForCards/>);
+    const linkElement = screen.getByText(/Text from card/i);
+    expect(linkElement).toBeInTheDocument();
+});
